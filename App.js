d1339947cbe01e61e609c85c5da4209dca9352a4
Venu-Gopaldr <144423258+Venu-Gopaldr@users.noreply.github.com>
2024-01-08 11:09:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Commentbox from './components/Commentbox';
+
+function App() {
+  return (
+    <div className="container">
+      <Commentbox name="Comments"/>
+   </div>
+  );
+}
+
+export default App;
---
+++ Commentbox.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+function Commentbox(props) {
+
+    const [comment, setcomment] = useState("   ")
+
+
+    const openTextbox=()=>{
+        
+    }
+    const removeContent=()=>{
+        let newComment = " " 
+        setcomment(newComment)
+    }
+    const handleOnChange=(event)=>{
+        setcomment(event.target.value)
+    }
+  return (
+    <>
+    <div>
+    <h1>{props.name}</h1>
+      <div className="mb-10">
+          <textarea className="form-control" id="text" rows="6" value={comment} onChange={handleOnChange}></textarea>
+      </div>
+      <button className="btn btn-primary mx-5" onClick={openTextbox}>Reply</button>
+      <button className="btn btn-primary mx-5" onClick={removeContent} >Delete</button>
+    </div>
+    {/* <div>
+        <h4>Preview</h4>
+        <p>{comment}</p>
+    </div> */}
+    </>
+  )
+}
+
+export default Commentbox
